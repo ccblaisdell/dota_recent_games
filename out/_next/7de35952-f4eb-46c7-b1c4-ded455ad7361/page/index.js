d499a9377208b7696d1027951227abd824d2753e
Colby Blaisdell <ccblaisdell@gmail.com>
2017-08-28 21:26:44 -0500

    Static site

---
+++ out/_next/7de35952-f4eb-46c7-b1c4-ded455ad7361/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([2],[],[401]);
+            return { page: comp.default }
+          })
+        
