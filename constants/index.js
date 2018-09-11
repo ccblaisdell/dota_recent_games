a930322f4c841a33e2bd130f1abf80c8aa7bf410
Colby Blaisdell <ccblaisdell@gmail.com>
2018-09-11 17:36:07 -0500

    Add dicer and farouk

--- constants/index.js
+++ constants/index.js
@@ -5,7 +5,9 @@ export const PLAYER_NAMES = {
   "71227969": "Uncle Ginger",
   "44427624": "hoplyte",
   "65500551": "losandro",
-  "44273131": "mgrif"
+  "44273131": "mgrif",
+  "378481299": "Dicer Drosha",
+  "106803260": "₣ΔŘØΩ0100"
 };
 
 export const PLAYER_IDS = [
@@ -15,5 +17,7 @@ export const PLAYER_IDS = [
   "71227969",
   "44427624",
   "65500551",
-  "44273131"
+  "44273131",
+  "378481299",
+  "106803260"
 ];
--- data/2018/index.js
+++ data/2018/index.js
@@ -4,6 +4,8 @@ import uncleGinger from "./71227969";
 import hoplyte from "./44427624";
 import losandro from "./65500551";
 import mgrif from "./44273131";
+import dicer from "./378481299";
+import farouk from "./106803260";
 
 export default {
   "6498909": dialuposaurus,
@@ -11,5 +13,7 @@ export default {
   "71227969": uncleGinger,
   "44427624": hoplyte,
   "65500551": losandro,
-  "44273131": mgrif
+  "44273131": mgrif,
+  "378481299": dicer,
+  "106803260": farouk
 };
