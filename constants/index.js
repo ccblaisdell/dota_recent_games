30cda33960d19b00b2394582cf280156914a774a
Colby Blaisdell <ccblaisdell@gmail.com>
2020-12-31 14:52:25 -0600

    ID gardening

--- constants/index.js
+++ constants/index.js
@@ -1,31 +1,16 @@
 export const PLAYER_NAMES = {
-  // "105605638": "Charlymurphyyy",
-  "6498909": "Dialuposaurus",
-  "63658494": "Roamin Ronin",
-  "71227969": "Uncle Ginger",
-  "44427624": "hoplyte",
-  "65500551": "losandro",
-  "44273131": "mgrif",
-  "378481299": "Dicer Drosha",
-  "106803260": "₣ΔŘØΩ0100",
-  "453065139": "Token",
-  "87245032": "TacocaT",
-  "24023735": "Skeeterbug",
-  "24024009": "Amberkins"
+  6498909: "Dialuposaurus",
+  63658494: "Roamin Ronin",
+  71227969: "Uncle Ginger",
+  44427624: "hoplyte",
+  65500551: "losandro",
+  44273131: "mgrif",
+  378481299: "Dicer Drosha",
+  106803260: "₣ΔŘØΩ0100",
+  87245032: "TacocaT",
+  887106584: "Shift",
+  165654431: "Skizzie",
+  85886699: "Token",
 };
 
-export const PLAYER_IDS = [
-  // "105605638",
-  "6498909",
-  "63658494",
-  "71227969",
-  "44427624",
-  "65500551",
-  "44273131",
-  "378481299",
-  "106803260",
-  "453065139",
-  "87245032",
-  "24023735",
-  "24024009"
-];
+export const PLAYER_IDS = Object.keys(PLAYER_NAMES);
